d1d9d6a73b8d0a1e43310e6aedfb07a1aabfb362
Mady <userfree761@gmail.com>
2023-05-20 06:46:01 -0700

    Illuminate porlor

---
+++ Js/app.js
@@ -0,0 +1,20 @@
+// const studio = document.getElementById('ourStudio');
+
+// studio.addEventListener('click', () => {
+//    let offSetScrollY = window.scroll;
+//    body.style.transition = `translateY(${offSetScrollY})`
+// })
+
+
+// const btn = document.querySelector('#scrollBtn');
+
+// btn.addEventListener('click', () =>{
+// })
+
+// window.addEventListener('scroll' , () => {
+//     console.log(window.scrollY)
+// })
+
+// function ScrollUp(){
+//     window.scrollTo(0,0)
+// }
